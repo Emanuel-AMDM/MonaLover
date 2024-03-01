18da60a43f07493540118d9a62a424e4ae8bfb33
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-03-01 17:24:08 -0300

    Update index.js

--- register/js/index.js
+++ register/js/index.js
@@ -4,10 +4,16 @@ function cadastrar(){
     var nickname = document.querySelector("input#nickname")
     var senha    = document.querySelector("input#senha")
     var resposta = document.querySelector("div#resposta")
+
     
     if(nome.value == '' || email.value == '' || nickname.value == '' || senha.value == ''){
         alert(`!Por favor, Preencha todos os campos!`)
     }else{
         resposta.innerHTML = `<strong>!Cadastrado com sucesso!</strong>`
     }
+    
+    nome.value = ''
+    email.value = ''
+    nickname.value = ''
+    senha.value = ''
 }
