117c8249b18109f6113c202f6abee230ba70b672
Emanuel Menezes <menezesemanuel007@gmail.com>
2024-03-01 11:55:35 -0300

    Create index.js

---
+++ login/js/index.js
@@ -0,0 +1,23 @@
+function logar(){
+    var res   = document.querySelector("div#resposta")
+    var login = document.querySelector("input#login")
+    var senha = document.querySelector("input#senha")
+    
+    if(login.value == 'manu' && senha.value == '123'){
+        res.innerHTML = `<strong>!Logado com sucesso!</strong>`
+    }
+    else if(login.value == '' || senha.value == ''){
+        alert(`!Preencha os campos abaixo!`)
+    }else{
+        res.innerHTML = `<strong>!Senha ou Login invalido!</strong>`
+    }
+}
+
+function mostrarSenha(){
+    var mostrarsenha = document.querySelector("input#mostrar")
+    var senha        = document.querySelector("input#senha")
+
+    if(mostrarsenha.value == 1){
+        senha.type = "text"
+    }
+}
